80080a50eb121f7519bae3385efc5c080265776e
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2025-07-19 09:34:42 -0300

    Delete ex005dicas.c

--- ex005dicas.c
+++
@@ -1,8 +0,0 @@
-#import<stdio.h>
-#import<stdlib.h>
-#import<time.h>
-void main(){
-    srand(time(NULL));
-    int n = rand() % 100;//gera numero entre 0 e 99
-    printf("Eu gerei o numero (%d)", n);
-}
