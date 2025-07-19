eb3e22119f4e933acda3d755220acd61539103a6
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2025-07-19 09:32:07 -0300

    Add files via upload

---
+++ M01/primeiro.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+printf("Olá mundo!");
+system("pause");
+}
---
+++ M01/variaveis.c
@@ -0,0 +1,10 @@
+#import <stdio.h>
+void main(){
+    unsigned int idade = 33;
+    float peso = 85.9;
+    char sexo = 'M';
+    char *nome = "Juvenal";
+    printf("%s é do sexo %c, tem %i anos e pesa %.2fKg", nome, sexo, idade, peso);
+
+
+}
