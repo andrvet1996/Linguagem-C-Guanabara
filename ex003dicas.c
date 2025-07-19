f026171f5ee8e596e2dc7bd348af60f1f50da382
A.L.F. SANTOS <80923022+andrvet1996@users.noreply.github.com>
2025-07-19 09:34:24 -0300

    Delete ex003dicas.c

--- ex003dicas.c
+++
@@ -1,9 +0,0 @@
-#import <stdio.h>
-#import <locale.h> //usada para números decimais
- void main(){
-     setlocale(LC_ALL,"Portuguese");
-     float n;
-     printf("Digite um numero real: ");
-     scanf("%f",&n);
-     printf("Voce digitou o numero %.1f", n);
- }
